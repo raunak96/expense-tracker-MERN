3e180d295415988d9d3fe6cb002f972fcba29241
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-08-01 19:29:08 +0530

    removed bug

--- config/db.js
+++ config/db.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 const connectToMongoose = async () => {
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
+        const conn = await mongoose.connect(
+            `mongodb+srv://rawn_user:${process.env.MONGO_URI}.mongodb.net/expense-tracker?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+            }
+        );
         console.log(`DB Connected: ${conn.connection.host}`.cyan.bold);
     } catch (error) {
         console.log("DB error", error.message);
